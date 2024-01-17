02d7326db88a697d77aa38ec950a68ae7b4ea2aa
Manuel Blanco <manuelblanco@uc.cl>
2024-01-16 21:31:25 -0300

    Update GPT API, app and gitIGNORE

--- Backend/vision_api.py
+++ Backend/vision_api.py
@@ -1,5 +1,9 @@
 from openai import OpenAI
 import Backend.parametros as p
+from PIL import Image
+import io
+import base64
+
 def analyze_image_with_gpt4(image_data_list, question):
     client = OpenAI(api_key=p.OPENAI_KEY)
     image_contents = [ {"type": "image_url", "image_url": f"data:image/jpeg;base64,{image_data}"} for image_data in image_data_list]
@@ -15,3 +19,21 @@ def analyze_image_with_gpt4(image_data_list, question):
         max_tokens=300,
     )
     return response
+
+
+# Subida de múltiples archivos para GPT-4
+
+def upload_multiple_files(uploaded_files, question):
+    image_data_list = []
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            buffered = io.BytesIO()
+            image.save(buffered, format="JPEG")
+            image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            image_data_list.append(image_data)
+
+    if image_data_list and question:
+        return analyze_image_with_gpt4(image_data_list, question)
+    else:
+        return None
--- Frontend/app.py
+++ Frontend/app.py
@@ -2,16 +2,14 @@ import io
 import streamlit as st
 from PIL import Image
 import base64
-import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
-from PIL import Image
 import numpy as np
 import openai
 import Backend.parametros as p
 from Backend.vision_api import analyze_image_with_gpt4
 from Backend.funcion_yolo import modelo_yolo
-
+from Backend.vision_api import upload_multiple_files
 from Backend.Vertex_AI import predict_vertex
 
 def preprocess_image(image, target_size):
@@ -24,33 +22,21 @@ def preprocess_image(image, target_size):
     return image
 
 st.title("Brain Check")
-
 model_choice = st.selectbox('Elige el modelo:', ('OpenCV', 'Vertex', 'GPT-4', 'YoloV8'))
 
-if model_choice in ['OpenCV', 'Vertex']:
-    # Subida de archivo para ambas APIs
+# Inicialización de la variable image
+image = None
+
+if model_choice in ['OpenCV', 'Vertex', 'YoloV8']:
     uploaded_file = st.file_uploader("Carga tu imagen aquí", type=["jpg", "png"])
 
-    model1 = load_model(p.Path_CV) #path
-
     if uploaded_file is not None:
-        # Mostrar la imagen cargada
         image = Image.open(uploaded_file)
         st.image(image, caption='Imagen cargada', use_column_width=True)
 
-        if model_choice == 'OpenCV':
-            # llamar a la funcion de predicción de OpenCV
-            processed_image = preprocess_image(image, target_size=(32, 32))
-            prediction = model1.predict(processed_image)
-            prediction = np.argmax(prediction, axis=1)
-            if prediction == 0:
-                st.write("The MRI image is classified as: No Tumor")
-            else:
-                st.write("The MRI image is classified as: Yes Tumor")
-    if model_choice == 'OpenCV':
-        # llamar a la funcion de predicción de OpenCV
+    if model_choice == 'OpenCV' and image is not None:
+        model1 = load_model(p.Path_CV)
         processed_image = preprocess_image(image, target_size=(32, 32))
-        model1 = load_model(p.Path_CV) #path
         prediction = model1.predict(processed_image)
         prediction = np.argmax(prediction, axis=1)
         if prediction == 0:
@@ -58,77 +44,60 @@ if model_choice in ['OpenCV', 'Vertex']:
         else:
             st.write("The MRI image is classified as: Yes Tumor")
 
-    if model_choice == 'Vertex':
-        # Utilizar la función predict_vertex para la predicción
+    if model_choice == 'Vertex' and image is not None:
         prediction_response = predict_vertex(uploaded_file)
-
-        # Mostrar los resultados de Vertex AI en la aplicación
         if prediction_response:
             for prediction in prediction_response:
                 if 'displayNames' in prediction and 'confidences' in prediction and 'ids' in prediction:
                     for displayName, id, confidence in zip(prediction['displayNames'], prediction['ids'], prediction['confidences']):
                         st.write(f"Resultado de Vertex AI: {displayName}, Confianza: {confidence:.2f}")
 
-    if model_choice == 'YoloV8':
+    if model_choice == 'YoloV8' and image is not None:
         modelo_yolo(image)
 
-    # Convertir la imagen a base64 para la API de OpenAI
-    buffered = io.BytesIO()
-    image.save(buffered, format="JPEG")
-    image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    if image is not None:
+        # Convertir la imagen a base64 para la API de OpenAI
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG")
+        image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    # Caja de texto para obtener la pregunta del usuario para GPT-4
-    user_question = st.text_input("Escribe tu pregunta sobre la imagen para GPT-4:")
+        # Caja de texto para obtener la pregunta del usuario para GPT-4
+        user_question = st.text_input("Escribe tu pregunta sobre la imagen para GPT-4:")
 
-    # Agregar system, con el rol, contexto, segmentacion del cerebro y ubicacion
-    # del tumor (codigo previo para encontrar ubicacion)
-
-    # Botón de envío
-    if st.button('Enviar pregunta'):
-        try:
-            # Realizar la petición a OpenAI
-            response = openai.chat.completions.create(
-                model="gpt-4-vision-preview",
-                messages=[
-                    {
-                        "role": "user",
-                        "content": [
-                            {"type": "text", "text": user_question},
-                            {"type": "image_url", "image_url": f"data:image/jpeg;base64,{image_data}"}
-                        ],
-                    }
-                ],
-                max_tokens=300,
-            )
-            # Mostrar los resultados de Vertex AI en la aplicación
-            if prediction_response:
-                for prediction in prediction_response:
-                    if 'displayNames' in prediction and 'confidences' in prediction and 'ids' in prediction:
-                        for displayName, id, confidence in zip(prediction['displayNames'], prediction['ids'], prediction['confidences']):
-                            st.write(f"Resultado de Vertex AI: {displayName}, Confianza: {confidence:.2f}")
-        if model_choice == 'YoloV8':
-            # Llamar a la función de predicción de YoloV8 y capturar la respuesta
-            pass
+        # Botón de envío
+        if st.button('Enviar pregunta'):
+            try:
+                # Realizar la petición a OpenAI
+                response = openai.chat.completions.create(
+                    model="gpt-4-vision-preview",
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "text", "text": user_question},
+                                {"type": "image_url", "image_url": f"data:image/jpeg;base64,{image_data}"}
+                            ],
+                        }
+                    ],
+                    max_tokens=300,
+                )
+                st.write("Respuesta de GPT-4 Vision:")
+                st.write(response.choices[0].message.content)
+            except Exception as e:
+                st.error("Error al procesar la solicitud: " + str(e))
 
 # Subida de múltiples archivos para GPT-4
 if model_choice == 'GPT-4':
-    uploaded_files = st.file_uploader("Carga tus imágenes aquí para GPT-4", type=["jpg", "png"], accept_multiple_files=True)
-    image_data_list = []
-    for uploaded_file in uploaded_files:
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            buffered = io.BytesIO()
-            image.save(buffered, format="JPEG")
-            image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
-            image_data_list.append(image_data)
-
+    uploaded_files = st.file_uploader("Chat with GPT-4 Vision", type=["jpg", "png"], accept_multiple_files=True)
     user_question = st.text_input("Escribe tu pregunta sobre la imagen para GPT-4:")
+
     if uploaded_files and user_question and st.button('Enviar pregunta a GPT-4'):
         try:
-            # Usar la función modify para realizar la petición a OpenAI con múltiples imágenes
-            response = analyze_image_with_gpt4(image_data_list, user_question)
-            # Mostrar la respuesta
-            st.write("Respuesta de GPT-4 Vision:")
-            st.write(response.choices[0].message.content)
+            response = upload_multiple_files(uploaded_files, user_question)
+            if response:
+                st.write("Respuesta de GPT-4 Vision:")
+                st.write(response.choices[0].message.content)
+            else:
+                st.error("Por favor, sube al menos una imagen y escribe una pregunta.")
         except Exception as e:
             st.error("Error al procesar la solicitud: " + str(e))
